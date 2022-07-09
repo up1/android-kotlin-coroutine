2894d16445a6326189d704239063dfa422d11c01
up1 <somkiat.p@gmail.com>
2022-07-10 01:08:03 +0700

    Working with parallel + async/await

--- app/src/main/java/com/example/myapplication/ui/single/SingleViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/single/SingleViewModel.kt
@@ -7,6 +7,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.myapplication.data.model.ApiUser
 import com.example.myapplication.data.api.ApiService
 import com.example.myapplication.util.Resource
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class SingleViewModel (
@@ -23,12 +25,18 @@ class SingleViewModel (
         viewModelScope.launch {
             users.postValue(Resource.loading(null))
             try {
-                val users01 = apiService.getUsers()
-                val moreUsers = apiService.getMoreUsers()
-                val all = mutableListOf<ApiUser>()
-                all.addAll(users01)
-                all.addAll(moreUsers)
-                users.postValue(Resource.success(all))
+                // Catch error in coroutine
+                coroutineScope {
+                    val users01 = async { apiService.getUsers() }
+                    val moreUsers = async { apiService.getMoreUsers() }
+                    val errors = async { apiService.getUsersWithError() }
+
+                    val all = mutableListOf<ApiUser>()
+                    all.addAll(users01.await())
+                    all.addAll(moreUsers.await())
+                    all.addAll(errors.await())
+                    users.postValue(Resource.success(all))
+                }
             } catch (e: Exception) {
                 users.postValue(Resource.error(e.toString(), null))
             }
